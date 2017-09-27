dd5107f85dfa3575f10a65e65fa95ab7a98600c3
judyta wlodarska <jdtwlodar@gmail.com>
2017-09-27 20:52:32 +0200

    finish

--- dist/js/controllers/searchCtrl.js
+++ dist/js/controllers/searchCtrl.js
@@ -1,23 +1,22 @@
-
-    app.controller('searchCtrl', ['$scope', '$http', function($scope, $http) {
-        $scope.data = null;
-        $scope.user = 'jdtwlodar';
-        $scope.errorbox = false;
-        $scope.getuser = function(user) {
-            $http.get('https://api.github.com/users/' + ($scope.user) + '/repos')
-                .then(function successCallback(response) {
-                    $scope.data = response.data;
-                    $scope.errorbox = false;
-                }, function errorCallback(response) {
-                    console.log('Błąd');
-                    $scope.errorbox = true;
-                });
-
-
-        };
+app.controller('searchCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.data = null;
+    $scope.user = 'jdtwlodar';
+    $scope.errorbox = false;
+    $scope.details = true;
+    $scope.getuser = (user) => {
+        $http.get('https://api.github.com/users/' + ($scope.user) + '/repos')
+            .then(function successCallback(response) {
+                $scope.data = response.data;
+                $scope.errorbox = false;
+                $scope.details = true;
+            }, function errorCallback(response) {
+                $scope.errorbox = true;
+                $scope.details = false;
+            });
+    };
+    $scope.getuser();
+    $scope.check = function() {
         $scope.getuser();
-        $scope.check = function() {
-            $scope.getuser();
-        };
+    };
 
-    }]);
+}]);
